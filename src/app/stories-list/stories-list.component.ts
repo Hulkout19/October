d9b09d57235557c3ed0e76677a12106c61f1fe79
sven broman <svenbroman99@gmail.com>
2023-10-25 14:36:53 -0400

    committing 10 25

---
+++ src/app/stories-list/stories-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Stories } from 'src/stories';
+
+
+@Component({
+  selector: 'app-stories-list',
+  templateUrl: './stories-list.component.html',
+  styleUrls: ['./stories-list.component.css']
+})
+export class StoriesListComponent {
+  
+
+}
--- src/app/story-card/story-card.component.ts
+++ src/app/story-card/story-card.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { Stories } from 'src/stories';
+import { STORY, findStoryById } from '../StoryList';
+import { findAllActiveStories } from '../StoryList';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-story-card',
   templateUrl: './story-card.component.html',
   styleUrls: ['./story-card.component.css'],
   standalone: true,
-  imports: [MatCardModule]
+  imports: [MatCardModule, NgFor, NgIf]
 
 })
-export class StoryCardComponent {
+export class StoryCardComponent{
 
+  story: Stories[] = STORY;
+
+  constructor() {
+
+    
+  }
 }
+
+

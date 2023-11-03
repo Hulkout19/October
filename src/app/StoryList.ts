805e2d337e6f47d96647328bab4abdd0d9e71f04
sven broman <svenbroman99@gmail.com>
2023-11-03 13:06:10 -0400

    another commit for 11/3

--- src/app/StoryList.ts
+++ src/app/StoryList.ts
@@ -3,7 +3,7 @@ export const STORY: any = [
         id: 1,
         title: "Designing Plans",
         description: "A story about an architect",
-        iconUrl: "/assets/bookcase.jpg",
+        iconUrl: "/October/assets/bookcase.jpg",
         active: true,
         storyUrl: "/architect"
     },
@@ -11,7 +11,7 @@ export const STORY: any = [
         id: 2,
         title: "A Line in the Sand",
         description: "A story about an giant",
-        iconUrl: "/assets/Arizona.jpg",
+        iconUrl: "/October/assets/Arizona.jpg",
         active: true,
         storyUrl: "/giants"
     },
@@ -19,7 +19,7 @@ export const STORY: any = [
         id: 3,
         title: "Agent Chaos",
         description: "A story about an agent",
-        iconUrl: "/assets/kitchen.jpg",
+        iconUrl: "/October/assets/kitchen.jpg",
         active: true,
         storyUrl: "/agent"
     },
